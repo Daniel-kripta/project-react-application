5136f3a7bcbae66d800f61398a4507e94367022e
Daniel-kripta <danielgz-profesional@outlook.es>
2026-04-20 14:26:22 +0100

    Linked CSS and implemented vitest base

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css"
 
 
 
 export default function Navbar() {
   return (
 
-    <nav>
-        <ul>
+    <nav className={styles.nav}>
+        <ul className={styles.navlist}>
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/search">Search Food</NavLink></li>
             <li><NavLink to="/savedfood">Saved Food</NavLink></li>
